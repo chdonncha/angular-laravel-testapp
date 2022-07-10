a4afcf5d42b8067a3afd7484aa560ffda0712ba3
donncha <chdonncha@gmail.com>
2022-07-10 19:30:08 +0100

    setup basic routing for frontend home page

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { UsersComponent } from './pages/users/users.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UsersComponent,
+    HttpClientModule,
   ],
   imports: [
     BrowserModule,
