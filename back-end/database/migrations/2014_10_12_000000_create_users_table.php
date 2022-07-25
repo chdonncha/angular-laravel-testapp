90548c440da8bf7d277d523583982378c7977424
donncha <chdonncha@gmail.com>
2022-07-25 23:40:57 +0100

    basic seeder working

--- back-end/database/migrations/2014_10_12_000000_create_users_table.php
+++ back-end/database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,6 @@ class CreateUsersTable extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email');
-            $table->string('gender');
         });
     }
 
--- back-end/database/seeders/UsersTableSeeder.php
+++ back-end/database/seeders/UsersTableSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class UsersTableSeeder extends Seeder
 {
@@ -13,6 +15,10 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('users')->insert([
+            'first_name' => Str::random(10),
+            'last_name'=> Str::random(10),
+            'email' => Str::random(10).'@gmail.com',
+        ]);
     }
 }
